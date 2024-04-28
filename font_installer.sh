94698a2419711637c00be9658a5991a41495db8c
Dominik Sánchez <4673828+dsancheznet@users.noreply.github.com>
2024-04-28 15:27:25 +0100

    Update font_installer.sh
    Changed the San Francisco Font installation

--- font_installer.sh
+++ font_installer.sh
@@ -13,33 +13,36 @@ mkdir ./tmp
 # Change to directory
 cd tmp
 
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Black.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Bold.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Heavy.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Light.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Medium.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Regular.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Semibold.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Thin.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Ultralight.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Bold.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-BoldItalic.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Heavy.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-HeavyItalic.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Light.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-LightItalic.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Medium.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-MediumItalic.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Regular.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-RegularItalic.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Semibold.otf"
+wget --no-check-certificate "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-SemiboldItalic.otf"
+
+mv *.otf ~/.local/share/fonts/
+
 # Download section
 wget --no-check-certificate "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/EnvyCodeR.zip"
 wget --no-check-certificate "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraMono.zip"
 wget --no-check-certificate "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Iosevka.zip"
 wget --no-check-certificate "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/IosevkaTerm.zip"
 wget --no-check-certificate "https://use.fontawesome.com/releases/v6.4.0/fontawesome-free-6.4.0-desktop.zip"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Black.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Bold.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Heavy.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Light.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Medium.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Regular.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Semibold.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Thin.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoDisplay-Ultralight.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Bold.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-BoldItalic.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Heavy.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-HeavyItalic.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Light.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-LightItalic.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Medium.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-MediumItalic.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Regular.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-RegularItalic.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Semibold.otf"
-wget --no-check-certificate --directory-prefix=~/.local/share/fonts/ "https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-SemiboldItalic.otf"
 
 # Extract all otf and ttf files to local font directory
 for F in *.zip; do unzip $F "*.[oOtT][tT][fF]" -d ~/.local/share/fonts/; rm $F; done

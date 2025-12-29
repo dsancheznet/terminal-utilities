ea7c69a1bd88205e81fbb8348dcc103800d08e2d
Dominik Sánchez <4673828+dsancheznet@users.noreply.github.com>
2025-12-29 17:40:05 +0000

    Update install_mediaserver.sh
    UPDATE:
    · Updated script to run on Debian Trixie
    · Added verbose comments to allow the user to follow along
    · Added installation of TBS Drivers Automatically

--- install_mediaserver.sh
+++ install_mediaserver.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-echo "Install script for a..."
-echo "                    _ _ "
-echo "                   | (_)"
-echo " _ __ ___   ___  __| |_  __ _ ___  ___ _ ____   _____ _ __ "
-echo "| '_ ` _ \ / _ \/ _` | |/ _` / __|/ _ \ '__\ \ / / _ \ '__|"
-echo "| | | | | |  __/ (_| | | (_| \__ \  __/ |   \ V /  __/ | "  
-echo "|_| |_| |_|\___|\__,_|_|\__,_|___/\___|_|    \_/ \___|_| "
-echo "Written by D.Sánchez 2023"
+echo "Install script for a local..."
+echo "▄▄   ▄▄ ▄▄▄▄▄ ▄▄▄▄  ▄▄  ▄▄▄   ▄▄▄▄ ▄▄▄▄▄ ▄▄▄▄  ▄▄ ▄▄ ▄▄▄▄▄ ▄▄▄▄  "
+echo "██▀▄▀██ ██▄▄  ██▀██ ██ ██▀██ ███▄▄ ██▄▄  ██▄█▄ ██▄██ ██▄▄  ██▄█▄ "
+echo "██   ██ ██▄▄▄ ████▀ ██ ██▀██ ▄▄██▀ ██▄▄▄ ██ ██  ▀█▀  ██▄▄▄ ██ ██ "                                                                 
+echo "                                       Written by D.Sánchez 2023 "
 
-# Script to install a TVHeadend + Jellyfin Mediaserver from a Debian "Bookworm" barebone installation ( no X )
-# This script has to be executed as root.
+# Script to install a TVHeadend + Jellyfin Mediaserver from a Debian "Trixie" barebone installation ( no X )
 
-# Install required packets
-apt install wget sudo ethtool hdparm mc python3-neovim curl neomutt unrar unzip unace git gcc g++ make patchutils libproc-processtable-perl linux-headers-`uname -r` firmware-linux-nonfree firmware-linux-misc fuse3
+# Install required packets ( updated: DEC2025 )
+sudo apt install wget sudo ethtool hdparm mc python3-neovim curl neomutt unrar unzip unace git gcc g++ make patchutils libproc-processtable-perl linux-headers-`uname -r` firmware-linux-nonfree vim-youcompleteme vim-snippets vim-scripts vim-minimap vim-link-vim vim-airline vim-airline-themes vim-addon-manager duf eza 
+
+# Install
 
 # Install tvheadend ( will be available on port 9981 once rebooted )
-curl -1sLf   'https://dl.cloudsmith.io/public/tvheadend/tvheadend/setup.deb.sh'   | sudo -E bash
+echo "Installing TVHEADEND repo"
+curl -1sLf 'https://dl.cloudsmith.io/public/tvheadend/tvheadend/setup.deb.sh'   | sudo -E bash
 
 # Install Jellyfin ( will be available on port 8096 once rebooted )
+echo "Installing JELLYFIN repo"
 curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
+
+# Install packets from repos
+echo "Installing packets"
+sudo apt install tvheadend
+
+# Install TBS Drivers
+echo "Installing and compiling tbs drivers"
+curl "https://raw.githubusercontent.com/dsancheznet/terminal-utilities/refs/heads/main/install_tbs_drivers.sh" | bash
